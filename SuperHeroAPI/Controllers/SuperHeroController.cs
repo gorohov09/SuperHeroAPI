20857fe03ebe8e08b1394d4d7aadf85f98b6cc78
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-06-26 16:32:02 +0300

    Создание API-контроллера супергероев

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/heroes")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet("all")]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            var heroes = new List<SuperHero>
+            {
+                new SuperHero { Id = 1, 
+                    Name = "Spider Man", 
+                    FirstName = "Peter", 
+                    LastName = "Parker", 
+                    Place = "New York City"
+                }
+            };
+
+            return Ok(heroes);
+        }  
+    }
+}
