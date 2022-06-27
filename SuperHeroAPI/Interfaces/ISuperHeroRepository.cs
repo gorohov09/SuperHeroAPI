d3f120836bf082de88f87ef5e821aef5382f630c
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-06-27 19:58:32 +0300

    Создание интерфейса для репозитория супергероев

---
+++ SuperHeroAPI/Interfaces/ISuperHeroRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace SuperHeroAPI.Interfaces
+{
+    public interface ISuperHeroRepository
+    {
+        IEnumerable<SuperHero> GetAll();
+
+        SuperHero Get(int id);
+
+        void Add(SuperHero hero);
+
+        bool Update(SuperHero hero);
+
+        bool Delete(int id);
+    }
+}
