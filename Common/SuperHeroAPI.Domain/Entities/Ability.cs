d1aa31a9bfc86fa133bdc17d8c77e483197ce747
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-06-28 22:16:24 +0300

    Добавление сущности - способность

---
+++ Common/SuperHeroAPI.Domain/Entities/Ability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroAPI.Domain.Entities
+{
+    public class Ability
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        public ICollection<SuperHero> SuperHeroes { get; set; } = new List<SuperHero>();
+    }
+}
