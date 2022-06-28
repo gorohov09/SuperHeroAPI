8545bcc631513310cd7fd2b91f8c31304ecdc2c8
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-06-28 22:16:37 +0300

    Добавление сущности - команда

---
+++ Common/SuperHeroAPI.Domain/Entities/SuperHeroTeam.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroAPI.Domain.Entities
+{
+    public class SuperHeroTeam
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public DateTimeOffset DateOfCreation { get; set; }
+
+        public ICollection<SuperHero> Heroes { get; set; } = new List<SuperHero>();
+    }
+}
