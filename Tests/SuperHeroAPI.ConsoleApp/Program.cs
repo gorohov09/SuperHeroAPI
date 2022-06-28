710f1e81ad321f689a278bb0be6bf75a6b4874d6
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-06-28 23:11:11 +0300

    Попробовал протестировать БД через приложение

---
+++ Tests/SuperHeroAPI.ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿//using SuperHeroAPI.DAL.Context;
+//using SuperHeroAPI.Domain.Entities;
+
+//using (DataContext db = new DataContext())
+//{
+//    var team = new SuperHeroTeam
+//    {
+//        Name = "Мстители",
+//        DateOfCreation = new DateTime(1998, 7, 9),
+//    };
+
+//    var ability1 = new Ability
+//    {
+//        Description = "Гениальный интеллект"
+//    };
+
+//    var ability2 = new Ability
+//    {
+//        Description = "Выдающийся изобретатель и инженер"
+//    };
+
+//    var ability3 = new Ability
+//    {
+//        Description = "Мастер рукопашного боя"
+//    };
+
+//    var hero = new SuperHero
+//    {
+//        Name = "Железный человек",
+//        RealName = "Тони",
+//        RealLastName = "Старк",
+//        Height = 185,
+//        Weight = 90,
+//        Type = "Человек",
+//        Place = "Лосанджелес",
+//        Abilities = new List<Ability>() { ability1, ability2, ability3 },
+//        Team = team,
+//    };
+
+//    db.SuperHeroes.Add(hero);
+//    db.SaveChanges();
+//}
+
+
+
+
