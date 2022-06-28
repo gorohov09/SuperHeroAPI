dfe01b8520f561d6b47d1aa8095856b4f3d8c01b
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-06-28 22:17:09 +0300

    Настройка БД при помощи Fluent API

--- Data/SuperHeroAPI.DAL/Context/DataContext.cs
+++ Data/SuperHeroAPI.DAL/Context/DataContext.cs
@@ -8,5 +8,29 @@ namespace SuperHeroAPI.DAL.Context
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
         public DbSet<SuperHero> SuperHeroes { get; set; }
+
+        public DbSet<Ability> Abilities { get; set; }
+
+        public DbSet<SuperHeroTeam> SuperHeroTeams { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SuperHero>().ToTable("SuperHeroes");
+            modelBuilder.Entity<Ability>().ToTable("Abilities");
+            modelBuilder.Entity<SuperHeroTeam>().ToTable("SuperHeroTeams");
+
+            modelBuilder.Entity<SuperHero>().Property(s => s.Name).IsRequired();
+            modelBuilder.Entity<SuperHeroTeam>().Property(t => t.Name).IsRequired();
+
+            modelBuilder.Entity<SuperHero>()
+                .HasOne(s => s.Team)
+                .WithMany(t => t.Heroes)
+                .HasForeignKey(s => s.SuperHeroTeamId);
+
+            modelBuilder.Entity<Ability>()
+                .HasMany(a => a.SuperHeroes)
+                .WithMany(s => s.Abilities)
+                .UsingEntity(j => j.ToTable("Heroes_Abilities"));
+        }
     }
 }
