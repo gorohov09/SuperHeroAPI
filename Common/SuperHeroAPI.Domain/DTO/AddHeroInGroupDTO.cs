89bd3895489898575221647f32f29ca6c3d3537b
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-06-29 23:50:04 +0300

    Создание DTO-модели для добавления персонажа в группу

---
+++ Common/SuperHeroAPI.Domain/DTO/AddHeroInGroupDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroAPI.Domain.DTO
+{
+    public class AddHeroInGroupDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public DateTimeOffset? DateOfCreation { get; set; }
+    }
+}
