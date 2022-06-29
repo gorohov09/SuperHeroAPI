4441639d84843a3fa2386fd5b1fefb261a6440b0
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-06-29 23:48:57 +0300

    Создание интерфейса для работы с группой

---
+++ API/SuperHeroAPI.Services/Interfaces/ISuperHeroTeamRepository.cs
@@ -0,0 +1,12 @@
+﻿using SuperHeroAPI.Domain.DTO;
+using SuperHeroAPI.Domain.Entities;
+
+namespace SuperHeroAPI.Services.Interfaces
+{
+    public interface ISuperHeroTeamRepository
+    {
+        Task<SuperHeroTeam> GetById(int id);
+
+        Task<bool> AddHeroInGroup(int heroId, AddHeroInGroupDTO group);
+    }
+}
