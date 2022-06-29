f42f161931b7044f30effe59a84e26236d06cd90
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-06-29 23:50:25 +0300

    Создание контроллера WebAPI, отвечающего за работу с группой

---
+++ API/SuperHeroAPI/Controllers/SuperHeroTeamController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI.Domain.DTO;
+using SuperHeroAPI.Services.Interfaces;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/groups")]
+    [ApiController]
+    public class SuperHeroTeamController : ControllerBase
+    {
+        private readonly ISuperHeroTeamRepository _SuperHeroTeamService;
+        private readonly IMapper _Mapper;
+
+        public SuperHeroTeamController(ISuperHeroTeamRepository SuperHeroTeamService, IMapper Mapper)
+        {
+            _SuperHeroTeamService = SuperHeroTeamService;
+            _Mapper = Mapper;
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetById(int Id)
+        {
+            var group = await _SuperHeroTeamService.GetById(Id);
+            if (group is null)
+                return NotFound();
+            var dto_group = _Mapper.Map<SuperHeroTeamDTO>(group);
+            return Ok(dto_group);
+        }
+
+        [HttpPost("add/{heroId}")]
+        public async Task<IActionResult> AddHeroInGroup(int heroId, [FromBody]AddHeroInGroupDTO group)
+        {
+            if (group is null)
+                return NotFound();
+            var result = await _SuperHeroTeamService.AddHeroInGroup(heroId, group);
+            return Ok(result);
+        }
+    }
+}
