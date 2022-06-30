d888232f5f2f7fb8afa6e460e9bae0953e93fb59
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-06-30 23:34:39 +0300

    Интерфейс для работы с суперспособностями

---
+++ API/SuperHeroAPI.Services/Interfaces/IAbilityRepository.cs
@@ -0,0 +1,12 @@
+﻿using SuperHeroAPI.Domain.Entities;
+
+namespace SuperHeroAPI.Services.Interfaces
+{
+    public interface IAbilityRepository
+    {
+        Task<IEnumerable<Ability>> GetAbilites();
+
+        Task<Ability> GetAbilityById(int id);
+
+    }
+}
